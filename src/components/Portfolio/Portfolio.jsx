1a5d5c8a62e68a78b7f5c12f8d6e5f39c92997b4
Shahrukh-source <sk1109697@gmail.com>
2023-12-25 06:55:41 -0800

    first commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -26,15 +26,16 @@ const Portfolio = () => {
 
 
         <div className={`flexCenter ${css.showCase}`}>
-        <a href="https://razorpay-tailwind-project.vercel.app/">Razorpay_With_Tailwind </a>
-        <a href="https://react-blog-site-with-contextapi.vercel.app/">React_BLOGSITE_CONTEXTAPI_Project </a>
-        <a href="https://react-modern-login-signin-project.vercel.app/signup">React_Modern_Login_SigninForm_Project </a>
-        <a href="https://react-card-second-project-with-props.vercel.app/">React Card Second Project</a>
-        <a href="https://react-card-filter-project-with-props-toastify-f29g.vercel.app/">React Card First Project</a>
-        <a href="https://tour-travels-miniproject-with-props-drilling.vercel.app/">React Plan with Love</a>
-        <a href="https://props-drilling-and-lifting-state-up-mini-project.vercel.app/">React Mini Project</a>
-        <a href="https://srkzone.netlify.app/">React Srk Zone</a>
-        <a href="https://github.com/Shahrukh-source?tab=repositories">My GitHub Profile</a>
+        <a className="same" href="https://javascript-html-css-password-generator-project-wihq.vercel.app/">JAVASCRIPT-HTML-CSS_PASSWORD_GENERATOR_PROJECT</a>
+        <a className="same" href="https://razorpay-tailwind-project.vercel.app/">Razorpay_With_Tailwind </a>
+        <a className="same" href="https://react-blog-site-with-contextapi.vercel.app/">React_BLOGSITE_CONTEXTAPI_Project </a>
+        <a className="same" href="https://react-modern-login-signin-project.vercel.app/signup">React_Modern_Login_SigninForm_Project </a>
+        <a className="same" href="https://react-card-second-project-with-props.vercel.app/">React Card Second Project</a>
+        <a className="same" href="https://react-card-filter-project-with-props-toastify-f29g.vercel.app/">React Card First Project</a>
+        <a className="same" href="https://tour-travels-miniproject-with-props-drilling.vercel.app/">React Plan with Love</a>
+        <a className="same" href="https://props-drilling-and-lifting-state-up-mini-project.vercel.app/">React Mini Project</a>
+        <a className="same" href="https://srkzone.netlify.app/">React Srk Zone</a>
+        <a className="same" href="https://github.com/Shahrukh-source?tab=repositories">My GitHub Profile</a>
 
         </div>
       </div>
